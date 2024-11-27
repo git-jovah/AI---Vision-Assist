01504b5ef708d4ffd9bc8ef39353964c3798c8be
git-jovah <167107708+git-jovah@users.noreply.github.com>
2024-11-27 20:27:26 +0530

    Update 2daily_Assist.py

--- Assisting_visually_/pages/2daily_Assist.py
+++ Assisting_visually_/pages/2daily_Assist.py
@@ -2,11 +2,11 @@ import streamlit as st
 from main import function,langch
 
 st.title("Personalized Assistance")
-image = st.file_uploader("upload image",type=["jpg","png","jpeg"])
+image = st.file_uploader("Extract text from image:",type=["jpg","png","jpeg"])
 if image:
     st.image(image=image)
     function(image,"auto")
 
-chat = st.chat_input("Enter message..")
+chat = st.chat_input("Chat here..")
 if chat:
     langch(chat)
